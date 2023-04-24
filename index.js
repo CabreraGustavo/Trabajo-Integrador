71e7f1a3b346cb8782e35fede09ff9ca74967366
Gustavo Cabrera <coseba.8@hotmail.com>
2023-04-23 21:46:42 -0300

    Cambios y se agrego el formulario a descargar

--- index.js
+++ index.js
@@ -1,92 +1,73 @@
-emailjs.init("OXWMo0u-x9pjLqy-J");
 $(document).ready(function() {
-   $("#enviar").click(function (){
 
-     if ($("#formulario")[0].checkValidity()) {
- 
-       let nombre = $("#nombre").val();
-       let email = $("#correo").val();
-       let mensaje = $("#mensaje").val()
- 
-       let templateParams = {
-         to_name: nombre ,
-         from_name: email ,
-         message_html: mensaje ,
-       };
- 
-       emailjs.send("service_hkqd7r8", "template_oexewxb", templateParams)
-         .then(function(response) {
-           alert("Mensaje enviado", response);
-         }, function(error) {
-           alert("Error", error);
-         });
-     } else {
-       alert("Por favor complete todos los campos requeridos.");
-     }
-   });
-      const formWizard = document.getElementById('form-wizard');
-      const sections = formWizard.querySelectorAll('.form-section');
-      const prevButton = formWizard.querySelectorAll('.prev-button');
-      const nextButton = formWizard.querySelectorAll('.next-button');
-      let currentSection = 0;
-      
-      function showSection(sectionIndex) {
-        sections[sectionIndex].style.display = 'block';
-      }
-      
-      function hideSection(sectionIndex) {
-        sections[sectionIndex].style.display = 'none';
-      }
-      
-      function validateForm() {
-        const inputs = sections[currentSection].querySelectorAll('input');
-        let isValid = true;
-      
-        for (let i = 0; i < inputs.length; i++) {
-          if (!inputs[i].checkValidity()) {
-            isValid = false;
-            break;
-          }
-        }
-      
-        return isValid;
-      }
-      
-      function navigateToSection(sectionIndex) {
-        if (validateForm()) {
-          hideSection(currentSection);
-          currentSection = sectionIndex;
-          showSection(currentSection);
-        }
-      }
-      
-      function handlePrevButtonClick() {
-        navigateToSection(currentSection - 1);
-      }
-      
-      function handleNextButtonClick() {
-        navigateToSection(currentSection + 1);
-      }
-      
-      function handleFormSubmit(event) {
-        event.preventDefault();
-        alert('Formulario enviado correctamente');
-      }
-      
-      for (let i = 1; i < sections.length; i++) {
-        hideSection(i);
-      }
-      
-      for (let i = 0; i < prevButton.length; i++) {
-        prevButton[i].addEventListener('click', handlePrevButtonClick);
-      }
-      
-      for (let i = 0; i < nextButton.length; i++) {
-        nextButton[i].addEventListener('click', handleNextButtonClick);
-      }
-      
-      formWizard.addEventListener('submit', handleFormSubmit);
-      
-      showSection(currentSection);
-      
+  $(".form-section").not(":first").hide();
+  $("#descarga").hide();
+
+  $("#job-application-form").validate();
+
+  $(".next-button").click(function() {
+    var currentSection = $(this).closest(".form-section");
+    var nextSection = currentSection.next(".form-section");
+    if ($("#job-application-form").valid()) {
+      currentSection.hide();
+      nextSection.show();
+    }
+  });
+
+  $(".previous-button").click(function() {
+    var currentSection = $(this).closest(".form-section");
+    var previousSection = currentSection.prev(".form-section");
+    currentSection.hide();
+    previousSection.show();
+  });
+
+  $("input,textarea").on("input", function() {
+    $(this).valid();
+  });
+
+  $("#job-application-form").submit(function(event) {
+    event.preventDefault();
+    if ($("#job-application-form").valid()) {
+      var summaryHtml = "<h2>Resumen de la solicitud</h2>";
+      summaryHtml += "<p><strong>Nombre:</strong> " + $("#name").val() + "</p>";
+      summaryHtml += "<p><strong>Correo electrónico:</strong> " + $("#email").val() + "</p>";
+      summaryHtml += "<p><strong>Puesto anterior:</strong> " + $("#job-title").val() + "</p>";
+      summaryHtml += "<p><strong>Descripción del trabajo:</strong> " + $("#job-description").val() + "</p>";
+      summaryHtml += "<p><strong>Nombre del referente:</strong> " + $("#ref-name").val() + "</p>";
+      summaryHtml += "<p><strong>Teléfono del referente:</strong> " + $("#ref-phone").val() + "</p>";
+      $("#summary").html(summaryHtml);
+      $("#descarga").show();
+    }
+  });
 });
+  function generatePDF() {
+    var nombre = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var ultimoTrabajo = document.getElementById('job-title').value;
+    var descripcionTrabajo = document.getElementById('job-description').value;
+    var nombreReferente = document.getElementById('ref-name').value;
+    var numeroReferente = document.getElementById('ref-phone').value;
+
+    var docDefinition = {
+      content: [
+        { text: 'Resumen de solicitud de empleo', style: 'header' },
+        { text: 'Nombre: ' + nombre },
+        { text: 'Correo electrónico: ' + email },
+        { text: 'Puesto anterior: ' + ultimoTrabajo },
+        { text: 'Descripción del trabajo: ' + descripcionTrabajo },
+        { text: 'Referencia anterior: ' + nombreReferente },
+        { text: 'Teléfono referencia: ' + numeroReferente }
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10]
+        }
+      }
+    };
+    pdfMake.createPdf(docDefinition).download('resumen-solicitud-empleo.pdf');
+  
+  };
+  
+
